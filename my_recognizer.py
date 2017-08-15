9385bb763383d70b2c994846c384a7374ab416ff
mylesieong <myles.bcm@gmail.com>
2017-08-15 12:44:18 +0800

    Pass the recognizer unittest

--- my_recognizer.py
+++ my_recognizer.py
@@ -18,14 +18,15 @@ def recognize(models: dict, test_set: SinglesData):
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    warnings.filterwarnings("ignore", category=RuntimeWarning)
+    
     probabilities = []
     guesses = []
 
-    # Calculate Probabilities
+    # Calculate Probabilities & guesses
     word_ids = test_set.get_all_sequences().keys() # e.g. [0,1,2,3]
     for wid in word_ids:
         recog_dict = {}
+
         for model_word in models.keys(): # e.g. ['FISH', 'JOHN', 'BREAD']
             model = models[model_word]
             item_X, item_lengths = test_set.get_item_Xlengths(wid)
@@ -33,14 +34,10 @@ def recognize(models: dict, test_set: SinglesData):
                 recog_dict[model_word] = model.score(item_X, item_lengths)
             except:
                 recog_dict[model_word] = float("-inf")
+
         probabilities.append(recog_dict)
-
-    # Calculate guesses
-    for wid in word_ids:
-        guesses.append(max(probabilities[wid]))
-
-    for wid in range(0,5):
-        print("For sequence {}, the probabilities dict is {} and the guess list is {}".format(test_set.get_item_sequences(wid), probabilities[wid], guesses[wid]))
+        guess = max(recog_dict, key=recog_dict.get)
+        guesses.append(guess)
 
     # return probabilities, guesses
     return (probabilities, guesses)
